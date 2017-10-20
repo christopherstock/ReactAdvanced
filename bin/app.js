70a24a7a33867aa8ef9de3db673ac50ed8970f0e
Christopher Stock <email@christopherstock.de>
2017-10-20 15:18:23 +0200

    Removed obsolete compiled js files.

--- bin/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-exports.__esModule = true;
-__export(require("./component/App"));
-__export(require("./component/TaskInput"));
-__export(require("./component/TaskList"));
-//# sourceMappingURL=app.js.map
